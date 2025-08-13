fd22f30d59a333005e5ed3980ac40cc02a1c50da
Brrusco <brucecchet@gmail.com>
2025-08-13 11:29:21 +0200

    init client server

--- src/client.c
+++ src/client.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "errExit.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
     printf("hello world Client\n");
+
+    char *path2ServerFIFO = "/tmp/myfifo";
+
+    printf("<Client> opening FIFO %s...\n", path2ServerFIFO);
+    // Open the FIFO in write-only mode
+    int serverFIFO = open(path2ServerFIFO, O_WRONLY);
+    if (serverFIFO == -1)
+        errExit("open failed");
+
+
+    // Close the FIFO
+    if (close(serverFIFO) != 0)
+        errExit("close failed");
+    
     exit(0);
 }
--- src/server.c
+++ src/server.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "errExit.h"
 
-int main() {
+#define LEN 256
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
 
     printf("hello world Server\n");
 
-    char *fname = "/tmp/myfifo";
-    int res = mkfifo(fname, S_IRUSR|S_IWUSR);
+    char *path2ServerFIFO = "/tmp/myfifo";
+    if (mkfifo(path2ServerFIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1)
+        errExit("mkfifo failed");
+
+    printf("<Server> FIFO %s created!\n", path2ServerFIFO);
 
     // Opening for reading only
-    int fd = open(fname, O_RDONLY);
-    
-    // reading bytes from fifo
-    char buffer[LEN];
-    read(fd, buffer, LEN);
+    int serverFIFO = open(path2ServerFIFO, O_RDONLY);
+    if (serverFIFO == -1)
+        errExit("open failed");
+
+    char filePath[LEN];
+    printf("<Server> waiting for vector [a,b]...\n");
+    // Reading 2 integers from the FIFO.
+    int bufferLettura = read(serverFIFO, &filePath[0], sizeof(filePath));
     
     // Printing buffer on stdout
-    printf("%s\n", buffer);
+    printf("%s\n", bufferLettura);
     
     // closing the fifo
-    close(fd);
+    close(serverFIFO);
     
     // Removing FIFO
-    unlink(fname);
+    unlink(path2ServerFIFO);
 
 
     exit(0);
 }
+
+// Returns 0 on success, or -1 on error
+int mkfifo(const char *pathname, mode_t mode);
